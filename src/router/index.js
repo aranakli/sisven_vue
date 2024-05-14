b8684b3434dd10df40feba700aae3e4eaddd770b
Brian Marin <brian200218@gmail.com>
2024-05-13 20:57:29 -0500

    Avance

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import EditCustomer from '../components/customer/EditCustomer.vue'
+import NewCustomer from '../components/customer/NewCustomer.vue'
 import Customer from '../views/Customer'
+import EditPaymode from '../components/paymode/EditPaymode.vue'
+import NewPaymode from '../components/paymode/NewPaymode.vue'
 import Paymode from '../views/Paymode'
 
 const routes = [
@@ -20,12 +24,32 @@ const routes = [
   {
     path: '/customer',
     name: 'Customer',
-    component: HomeView
+    component: Customer
+  },
+  {
+    path: '/add-customer',
+    name: 'NewCustomer',
+    component: NewCustomer
+  },
+  {
+    path: '/editar-customer/:id',
+    name: 'EditCustomer',
+    component: EditCustomer
+  },
+  {
+    path: '/add-paymode',
+    name: 'NewPaymode',
+    component: NewPaymode
+  },
+  {
+    path: '/editar-paymode/:id',
+    name: 'EditPaymode',
+    component: EditPaymode
   },
   {
     path: '/paymode',
     name: 'Paymode',
-    component: HomeView
+    component: Paymode
   },
 ]
 
